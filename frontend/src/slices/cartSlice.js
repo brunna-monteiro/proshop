d346185bbe5ed11c681060de11ea441836d3cff5
brunna monteiro <brunnabrauner@gmail.com>
2024-02-21 18:41:57 +0100

    Refactor cartSlice to cartUtils and added visual number of items in the shopping cart

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -1,9 +1,8 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { updateCart } from "../utils/cartUtils";
 
 const initialState = localStorage.getItem("cart")
 ? JSON.parse(localStorage.getItem("cart")) : {cartItems: []};
-
-const addDecimals = (num) => (Math.round(num * 100) /100).toFixed(2)
 
 const cartSlice = createSlice({
   name: "cart",
@@ -21,23 +20,7 @@ const cartSlice = createSlice({
         state.cartItems = [...state.cartItems, item]
       }
 
-      //Caculate items price
-      state.itemsPrice = addDecimals(state.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0));
-
-      //Caculate shipping price (Order over $100: free, otherwise $10)
-      state.shippingPrice = addDecimals(state.itemsPrice > 100 ? 0 : 10);
-
-      //Caculate tax price (15% tax)
-      state.taxPrice = addDecimals(Number((0.15 * state.itemsPrice).toFixed(2)));
-
-      //Caculate total price
-      state.totalPrice = (
-        Number(state.itemsPrice) +
-        Number(state.shippingPrice) +
-        Number(state.taxPrice)
-      ).toFixed(2);
-
-      localStorage.setItem('cart', JSON.stringify(state));
+      return updateCart(state);
     },
   }
 });
