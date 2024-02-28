c06726a03543c6eca41f4eace9c9cb99fe10529d
brunna monteiro <brunnabrauner@gmail.com>
2024-02-28 18:31:54 +0100

    Created logout user func clearing cookies and register new user func with encoded password

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,5 @@
-import express from "express";
-const router = express.Router();
+import express from "express"
+const router = express.Router()
 import {
   authUser,
   registerUser,
@@ -10,12 +10,13 @@ import {
   getUserById,
   deleteUser,
   updateUser
-} from '../controllers/userController.js';
+} from "../controllers/userController.js"
+import { protect, admin } from "../middleware/authMiddleware.js"
 
-router.route('/').post(registerUser).get(getUsers);
-router.post('/logout', logoutUser);
-router.post('/login', authUser);
-router.route('/profile').get(getUserProfile).put(updateUserProfile);
-router.route('/:id').get(getUserById).delete(deleteUser).put(updateUser)
+router.route('/').post(registerUser).get(protect, admin, getUsers)
+router.post('/logout', logoutUser)
+router.post('/login', authUser)
+router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile)
+router.route('/:id').get(protect, admin, getUserById).delete(protect, admin, deleteUser).put(protect, admin, updateUser)
 
-export default router;
+export default router
